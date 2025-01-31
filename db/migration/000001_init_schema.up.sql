c389a4c03a8d6a16f058c40f14d3a2d482676003
carlosjorge <carlosjorgevd@gmail.com>
2025-01-31 16:42:02 -0300

    initial commit

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "account" (
+    "id" bigserial PRIMARY KEY,
+    "owner" varchar NOT NULL,
+    "balance" bigint NOT NULL,
+    "currency" varchar NOT NULL,
+    "created_at" timestamp DEFAULT (now ())
+);
+
+CREATE TABLE "entries" (
+    "id" bigserial PRIMARY KEY,
+    "account_id" bigint NOT NULL,
+    "amount" bigint NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (now ())
+);
+
+CREATE TABLE "transfers" (
+    "id" bigserial PRIMARY KEY,
+    "from_account" bigint NOT NULL,
+    "to_account" bigint NOT NULL,
+    "amount" bigint NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT (now ())
+);
+
+CREATE INDEX ON "account" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account");
+
+CREATE INDEX ON "transfers" ("to_account");
+
+CREATE INDEX ON "transfers" ("from_account", "to_account");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negativve or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'it must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account") REFERENCES "account" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account") REFERENCES "account" ("id");
